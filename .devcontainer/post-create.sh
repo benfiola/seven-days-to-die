3fac68c834794a26eed04363621d6ce1801b36ad
Ben Fiola <benfiola@gmail.com>
2025-01-31 21:00:00 -0800

    chore: remove unused devcontainer post-create scripts

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 set -e
 
-export DEBIAN_FRONTEND=noninteractive
-
-# install add-apt-repository
-apt -y update
-apt -y install software-properties-common
-
-# install steamcmd
-add-apt-repository -y multiverse
-dpkg --add-architecture i386
-apt -y update
-echo steam steam/question select "I AGREE" | debconf-set-selections
-echo steam steam/license note '' | debconf-set-selections
-apt -y install steamcmd
-ln -s /usr/games/steamcmd /usr/local/bin/steamcmd
-
-unset DEBIAN_FRONTEND
+make install-depot-downloader
